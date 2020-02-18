e49db3f0c9d36457dfeb52040fed61f5e21b6e49
Alexey Litovchenko <alex.lit@outlook.com>
2020-02-18 13:01:06 +0300

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  import QuoteBox from '@/components/quote-box';
+
+  @Component<App>({
+    components: { QuoteBox },
+  })
+  export default class App extends Vue {}
+</script>
+
+<template>
+  <main class="app">
+    <quote-box></quote-box>
+  </main>
+</template>
+
+<style lang="postcss" scoped>
+  .app {
+    display: flex;
+    min-width: 800px;
+    height: 100vh;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
